454bc248cd3cbd5d86851b433188a57823e22b77
LucaStabo <luca.staboli@studenti.unitn.it>
2020-12-11 18:26:40 +0100

    new version

--- VagrantTopologyOSPF.py
+++ VagrantTopologyOSPF.py
@@ -1,3 +1,4 @@
+import ipcalc 
 
 #this function writes the beginning of the VagrantFile
 def BeginVagrantFile(f):
@@ -22,32 +23,42 @@ def BeginVagrantFile(f):
 #this function write in the vagrant file a new PC host
 def writeHost(f,Host):
 
-    #extrapolate each attribute from the touples
-    Name = Host[1]["Name"]
-    Os  = Host[1]["Os"]
-    Ram = Host[1]["Ram"]
-    Ip  = Host[1]["Ip"]
-    Netmask = Host[1]["Netmask"]
-    IpTrn = Ip.split(".")[0] + "." + Ip.split(".")[1] + "." + Ip.split(".")[2]
-
     print("adding an host to the vagrant file")
+
+    #extrapolate each attribute from the touples
+    Id = Host[1]["Id"]
+    Name = Host[1]["Name"]
+    Ram = Host[1]["Ram"]
+    Os  = Host[1]["Os"]
+    
+    Ip = Host[1]["Network"][0]["Ip"]
+    Netmask = Host[1]["Network"][0]["Netmask"]
+    Interface = Host[1]["Network"][0]["Interface"]
+    IpNoSub = Ip.split("/")[0]
+
+    Network = ipcalc.Network(Ip)
+    IpNet = Network.network()
+
+    #there must be a more efficient way to calculate this, this one is too trivial
+    for x in Network:
+      Gateway = str(x)
 
     f.write("config.vm.define \"" + Name + "\" do |" + Name + "|\n")
     f.write(Name + ".vm.box = \"" + Os + "\"\n")
     f.write(Name + ".vm.hostname = \"" + Name + "\"\n")
 
-    if Host[0] is 0:
-        f.write(Name + ".vm.network \"private_network\", ip: \"" + Ip +"\", netmask: \"" + Netmask + "\", virtualbox__intnet: \"broadcast_router-south-1\", auto_config: true\n")
-    if Host[0] is 1:
-        f.write(Name + ".vm.network \"private_network\", ip: \"" + Ip +"\", netmask: \"" + Netmask + "\", virtualbox__intnet: \"broadcast_router-south-2\", auto_config: true\n")
-    if Host[0] is 2:
-        f.write(Name + ".vm.network \"private_network\", ip: \"" + Ip +"\", netmask: \"" + Netmask + "\", virtualbox__intnet: \"broadcast_router-south-3\", auto_config: true\n")
+    if Id is 1:
+        f.write(Name + ".vm.network \"private_network\", ip: \"" + IpNoSub +"\", netmask: \"" + Netmask + "\", virtualbox__intnet: \"broadcast_router-south-1\", auto_config: true\n")
+    if Id is 2:
+        f.write(Name + ".vm.network \"private_network\", ip: \"" + IpNoSub +"\", netmask: \"" + Netmask + "\", virtualbox__intnet: \"broadcast_router-south-2\", auto_config: true\n")
+    if Id is 3:
+        f.write(Name + ".vm.network \"private_network\", ip: \"" + IpNoSub +"\", netmask: \"" + Netmask + "\", virtualbox__intnet: \"broadcast_router-south-3\", auto_config: true\n")
     
     
     f.write(Name + ".vm.provision \"shell\", run: \"always\", inline: <<-SHELL\n")
     f.write("echo \"Static Routig configuration Started for " + Name + "\"\n")
     f.write("sudo sysctl -w net.ipv4.ip_forward=1\n")
-    f.write("sudo route add -net " + IpTrn+ ".0 netmask " + Netmask + " gw " + IpTrn+ ".254 dev eth1\n")
+    f.write("sudo route add -net " + str(IpNet) + " netmask " + Netmask + " gw " + Gateway + " dev " + Interface + "\n")
     f.write("echo \"Configuration END\"\n")
     f.write("echo \"" + Name + " is ready to Use\"\n")
     f.write("SHELL\n")
@@ -62,32 +73,62 @@ def writeHost(f,Host):
 #this function write in the vagrant file a new Router
 def writeRouter(f,Router):
 
+    print("adding a router to the vagrant file") 
+
     #extrapolate each attribute from the touples
+    Id = Router[1]["Id"]
     Name = Router[1]["Name"]
     Ram = Router[1]["Ram"]
     Os  = Router[1]["Os"]
-    IpEth1 = Router[1]["IpEth1"]
-    IpEth2 = Router[1]["IpEth2"]
-    IpEth3 = Router[1]["IpEth3"]
-    IpEth1Trn = IpEth1.split(".")[0] + "." + IpEth1.split(".")[1] + "." + IpEth1.split(".")[2]
-    IpEth2Trn = IpEth2.split(".")[0] + "." + IpEth2.split(".")[1] + "." + IpEth2.split(".")[2]
-    IpEth3Trn = IpEth3.split(".")[0] + "." + IpEth3.split(".")[1] + "." + IpEth3.split(".")[2]
 
-    print("adding a router to the vagrant file")
+    Ip1 = Router[1]["Network"][0]["Ip"]
+    Netmask1 = Router[1]["Network"][0]["Netmask"]
+    Interface1 = Router[1]["Network"][0]["Interface"]
+    IpNoSub1 = Ip1.split("/")[0]
+    NetmaskAbbr1 = Ip1.split("/")[1]
+
+    Ip2 = Router[1]["Network"][1]["Ip"]
+    Netmask2 = Router[1]["Network"][1]["Netmask"]
+    Interface2 = Router[1]["Network"][1]["Interface"]
+    IpNoSub2 = Ip2.split("/")[0]
+    NetmaskAbbr2 = Ip2.split("/")[1]
+
+    Ip3 = Router[1]["Network"][2]["Ip"]
+    Netmask3 = Router[1]["Network"][2]["Netmask"]
+    Interface3 = Router[1]["Network"][2]["Interface"]
+    IpNoSub3 = Ip3.split("/")[0]
+    NetmaskAbbr3 = Ip3.split("/")[1]
+    
+    Network1 = ipcalc.Network(Ip1)
+    IpNet1 = Network1.network()
+    for x in Network1:
+      Gateway1 = str(x)
+
+    Network2 = ipcalc.Network(Ip2)
+    IpNet2 = Network2.network()
+    for x in Network2:
+      Gateway2 = str(x)
+
+    Network3 = ipcalc.Network(Ip3)
+    IpNet3 = Network3.network()
+    for x in Network3:
+      Gateway3 = str(x)     
+
+
 
     f.write("config.vm.define \""+ Name +"\" do |" + Name + "|\n")
     f.write(Name + ".vm.box = \"" + Os + "\"\n")
     f.write(Name + ".vm.hostname = \""+ Name +"\"\n")
 
-    if Router[0] is 3:
+    if Id is 4:
         f.write(Name + ".vm.network \"private_network\", virtualbox__intnet: \"broadcast_router-south-1\", auto_config: false\n")
         f.write(Name + ".vm.network \"private_network\", virtualbox__intnet: \"broadcast_router-inter-1\", auto_config: false\n")
         f.write(Name + ".vm.network \"private_network\", virtualbox__intnet: \"broadcast_router-inter-3\", auto_config: false\n")
-    if Router[0] is 4:
+    if Id is 5:
         f.write(Name + ".vm.network \"private_network\", virtualbox__intnet: \"broadcast_router-south-2\", auto_config: false\n")
         f.write(Name + ".vm.network \"private_network\", virtualbox__intnet: \"broadcast_router-inter-2\", auto_config: false\n")
         f.write(Name + ".vm.network \"private_network\", virtualbox__intnet: \"broadcast_router-inter-1\", auto_config: false\n")
-    if Router[0] is 5:
+    if Id is 6:
         f.write(Name + ".vm.network \"private_network\", virtualbox__intnet: \"broadcast_router-south-3\", auto_config: false\n")
         f.write(Name + ".vm.network \"private_network\", virtualbox__intnet: \"broadcast_router-inter-3\", auto_config: false\n")
         f.write(Name + ".vm.network \"private_network\", virtualbox__intnet: \"broadcast_router-inter-2\", auto_config: false\n")  
@@ -110,18 +151,18 @@ def writeRouter(f,Router):
     f.write("sudo vtysh -c '\n")
     f.write("configure terminal\n")
     f.write("router ospf\n")
-    f.write("network " + IpEth1Trn + ".0/24 area 0.0.0.0\n")
-    f.write("network " + IpEth2Trn + ".0/24 area 0.0.0.0\n") 
-    f.write("network " + IpEth3Trn + ".0/24 area 0.0.0.0\n") 
+    f.write("network " + str(IpNet1) + "/" + NetmaskAbbr1 + " area 0.0.0.0\n")
+    f.write("network " + str(IpNet2) + "/" + NetmaskAbbr2 + " area 0.0.0.0\n") 
+    f.write("network " + str(IpNet3) + "/" + NetmaskAbbr3 + " area 0.0.0.0\n") 
     f.write("exit\n")
-    f.write("interface eth1\n")
-    f.write("ip address " + IpEth1 + "/24\n")
+    f.write("interface " + Interface1 + "\n")
+    f.write("ip address " + IpNoSub1 + "/" + NetmaskAbbr1 + "\n")
     f.write("exit\n")
-    f.write("interface eth2\n")
-    f.write("ip address " + IpEth2 + "/24\n")
+    f.write("interface " + Interface2 + "\n")
+    f.write("ip address " + IpNoSub2 + "/" + NetmaskAbbr2 + "\n")
     f.write("exit\n")
-    f.write("interface eth3\n")
-    f.write("ip address " + IpEth3 + "/24\n")
+    f.write("interface " + Interface3 + "\n")
+    f.write("ip address " + IpNoSub3 + "/" + NetmaskAbbr3 + "\n")
     f.write("do write\n")
     f.write("exit\n")
     f.write("exit\n")
@@ -141,63 +182,107 @@ def writeRouter(f,Router):
 #the following is a fake graph that i used for testing
 #instead of typing everytime the input in the command line
 host1 = (1,{
-  "Name":"hostbanana",
+  "Id" : 1,
+  "Name":"host1",
   "Type": "Host",
   "Ram": "1024",
   "Os": "bento/ubuntu-16.04",
-  "Ip": "192.168.1.1",
-  "Netmask": "255.255.255.0"
+  "Network" : [{
+    "Ip": "192.168.1.1/24",
+    "Netmask": "255.255.255.0",
+    "Interface" : "eth1"
+  }]
 })
 host2 = (2,{
-  "Name":"hostmela",
+  "Id" : 2,
+  "Name":"host2",
   "Type": "Host",
   "Ram": "1024",
   "Os": "bento/ubuntu-16.04",
-  "Ip": "192.168.2.1",
-  "Netmask": "255.255.255.0"
+  "Network" : [{
+    "Ip": "192.168.2.1/24",
+    "Netmask": "255.255.255.0",
+    "Interface" : "eth1"
+  }]
 })
 host3 = (3,{
-  "Name":"hostpera",
+  "Id" : 3,
+  "Name":"host3",
   "Type": "Host",
   "Ram": "1024",
   "Os": "bento/ubuntu-16.04",
-  "Ip": "192.168.3.1",
-  "Netmask": "255.255.255.0"
+  "Network" : [{
+    "Ip": "192.168.3.1/24",
+    "Netmask": "255.255.255.0",
+    "Interface" : "eth1"
+  }]
 })
 
 rout1 = (4,{
-  "Name":"routerjack",
+  "Id" : 4,
+  "Name":"router1",
   "Type": "Router",
   "Ram": "1024",
   "Os": "bento/ubuntu-16.04",
-  "IpEth1": "192.168.1.254",
-  "IpEth2": "192.168.100.1",
-  "IpEth3": "192.168.101.2"
+  "Network" : [{
+    "Ip": "192.168.1.254/24",
+    "Netmask": "255.255.255.0",
+    "Interface" : "eth1"
+  },{
+    "Ip": "192.168.100.1/24",
+    "Netmask": "255.255.255.0",
+    "Interface" : "eth2"
+  },{
+    "Ip": "192.168.101.2/24",
+    "Netmask": "255.255.255.0",
+    "Interface" : "eth3"
+  }]
 })
 rout2 = (5,{
-  "Name":"routersteve",
+  "Id" : 5,
+  "Name":"router2",
   "Type": "Router",
   "Ram": "1024",
   "Os": "bento/ubuntu-16.04",
-  "IpEth1": "192.168.2.254",
-  "IpEth2": "192.168.100.2",
-  "IpEth3": "192.168.102.2"
+  "Network" : [{
+    "Ip": "192.168.2.254/24",
+    "Netmask": "255.255.255.0",
+    "Interface" : "eth1"
+  },{
+    "Ip": "192.168.100.2/24",
+    "Netmask": "255.255.255.0",
+    "Interface" : "eth2"
+  },{
+    "Ip": "192.168.102.2/24",
+    "Netmask": "255.255.255.0",
+    "Interface" : "eth3"
+  }]
 })
 rout3 = (6,{
-  "Name":"ruoterjhon",
+  "Id" : 6,
+  "Name":"ruoter3",
   "Type": "Router",
   "Ram": "1024",
   "Os": "bento/ubuntu-16.04",
-  "Ip": "192.168.1.1",
-  "IpEth1": "192.168.3.254",
-  "IpEth2": "192.168.101.1",
-  "IpEth3": "192.168.102.1"
+  "Network" : [{
+    "Ip": "192.168.3.254/24",
+    "Netmask": "255.255.255.0",
+    "Interface" : "eth1"
+  },{
+    "Ip": "192.168.101.1/24",
+    "Netmask": "255.255.255.0",
+    "Interface" : "eth2"
+  },{
+    "Ip": "192.168.102.1/24",
+    "Netmask": "255.255.255.0",
+    "Interface" : "eth3"
+  }]
 })
 
 fakeNet = [host1,host2,host3,rout1,rout2,rout3]
 
 def main():
-    VagrantFile = open("VagrantfileOFFICIALE", "w")
+    VagrantFile = open("VagrantfileOSPF", "w")
 
     #read the data structure from input
     #Network = G.nodes.data():
