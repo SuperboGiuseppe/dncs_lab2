425d195cd418e37095f333c363f0608c22109dd2
Giuseppe Superbo <giuseppe.superbo97@gmail.com>
2021-02-14 18:36:48 +0100

    Bandwidth functionality add in OSPF template

--- fromHTMLtoVagrant/VagrantTopologyOSPF.py
+++ fromHTMLtoVagrant/VagrantTopologyOSPF.py
@@ -23,19 +23,27 @@ def BeginVagrantFile(f):
 #this function write in the vagrant file a new PC host
 def writeHost(f,Host, edges):
 
-    Id = Host[1]["Id"]
-    Name = Host[1]["Name"]
-    Ram = Host[1]["Ram"]
-    Os  = Host[1]["Os"]
-    CustumScript = Host[1]["custom_script"]
-    
-    Ip = Host[1]["Network"][0]["Ip"]
-    Netmask = Host[1]["Network"][0]["Netmask"]
-    Interface = Host[1]["Network"][0]["Interface"]
-    IpNoSub = Ip.split("/")[0]
+    Id = Host["id"]
+    Name = Host["label"]
+    Os  = Host["vm_image"]
+    Ram = Host"ram"]
+    N_Cpus = Host["n_cpus"]
+    Ip = Host["network_interfaces"][0]["ip_address"]
+    Netmask = Host["network_interfaces"][0]["netmask"]
+    Interface = Host["network_interfaces"][0]["name_interface"]
+    EdgeReference = Host["network_interfaces"][0]["edge"]
+    UplinkBandwidth = 0
+    DownlinkBandwidth = 0
+    for edge in edges:
+      if EdgeReference[0] == edge["from"] and EdgeReference[1] == edge["to"]:
+        UplinkBandwidth = edge["bandwidth_up"]
+        DownlinkBandwidth = edge["bandwidth_down"]
 
+    IpNoSub = Ip.split("/")[0]
     Network = ipcalc.Network(Ip)
     IpNet = Network.network()
+
+    CustumScript = Host["custom_script"]
 
     for x in Network:
       Gateway = str(x)
@@ -58,9 +66,21 @@ def writeHost(f,Host, edges):
     f.write(CustumScript + " \n")#here there is the custum script
     f.write("echo \"Configuration END\"\n")
     f.write("echo \"" + Name + " is ready to Use\"\n")
+    f.write('cd /home/vagrant\n')
+    f.write('git clone https://github.com/magnific0/wondershaper.git\n')
+    f.write('cd wondershaper\n')
+    for edge in edges:
+      if UplinkBandwidth > 0 or DownlinkBandwidth > 0:
+        f.write('sudo ./wondershaper -a ' + Interface)
+        if DownlinkBandwidth > 0:
+          f.write(' -d ' + str(DownlinkBandwidth))
+        if UplinkBandwidth > 0:
+          f.write(' -u ' + str(UplinkBandwidth))
+        f.write('\n')
     f.write("SHELL\n")
     f.write(Name + ".vm.provider \"virtualbox\" do |vb|\n")
     f.write("vb.memory = " + Ram + "\n")
+    f.write("vb.cpus = " + N_Cpus + "\n")
     f.write("end\n")
     f.write("end\n")
 
@@ -70,27 +90,48 @@ def writeHost(f,Host, edges):
 #this function write in the vagrant file a new Router
 def writeRouter(f,Router, edges):
 
-    Id = Router[1]["Id"]
-    Name = Router[1]["Name"]
-    Ram = Router[1]["Ram"]
-    Os  = Router[1]["Os"]
-    CustumScript = Router[1]["custom_script"]
+    Id = Router["id"]
+    Name = Router["label"]
+    Os  = Router["vm_image"]
+    Ram = Router["ram"]
+    N_Cpus = Router["n_cpus"]
 
-    Ip1 = Router[1]["Network"][0]["Ip"]
-    Netmask1 = Router[1]["Network"][0]["Netmask"]
-    Interface1 = Router[1]["Network"][0]["Interface"]
+    Ip1 = Router["network_interfaces"][0]["ip_address"]
+    Netmask1 = Router["network_interfaces"][0]["netmask"]
+    Interface1 = Router["network_interfaces"][0]["name_interface"]
+    EdgeReference1 = Router["network_interfaces"][0]["edge"]
+    UplinkBandwidth1 = 0
+    DownlinkBandwidth1 = 0
+    for edge in edges:
+      if EdgeReference1[0] == edge["from"] and EdgeReference1[1] == edge["to"]:
+        UplinkBandwidth1 = edge["bandwidth_up"]
+        DownlinkBandwidth1 = edge["bandwidth_down"]
     IpNoSub1 = Ip1.split("/")[0]
     NetmaskAbbr1 = Ip1.split("/")[1]
 
-    Ip2 = Router[1]["Network"][1]["Ip"]
-    Netmask2 = Router[1]["Network"][1]["Netmask"]
-    Interface2 = Router[1]["Network"][1]["Interface"]
+    Ip2 = Router["network_interfaces"][1]["ip_address"]
+    Netmask2 = Router["network_interfaces"][1]["netmask"]
+    Interface2 = Router["network_interfaces"][1]["name_interface"]
+    EdgeReference2 = Router["network_interfaces"][1]["edge"]
+    UplinkBandwidth2 = 0
+    DownlinkBandwidth2 = 0
+    for edge in edges:
+      if EdgeReference2[0] == edge["from"] and EdgeReference2[1] == edge["to"]:
+        UplinkBandwidth2 = edge["bandwidth_up"]
+        DownlinkBandwidth2 = edge["bandwidth_down"]
     IpNoSub2 = Ip2.split("/")[0]
     NetmaskAbbr2 = Ip2.split("/")[1]
 
-    Ip3 = Router[1]["Network"][2]["Ip"]
-    Netmask3 = Router[1]["Network"][2]["Netmask"]
-    Interface3 = Router[1]["Network"][2]["Interface"]
+    Ip3 = Router["network_interfaces"][2]["ip_address"]
+    Netmask3 = Router["network_interfaces"][2]["netmask"]
+    Interface3 = Router["network_interfaces"][2]["name_interface"]
+    EdgeReference3 = Router["network_interfaces"][2]["edge"]
+    UplinkBandwidth3 = 0
+    DownlinkBandwidth3 = 0
+    for edge in edges:
+      if EdgeReference3[0] == edge["from"] and EdgeReference3[1] == edge["to"]:
+        UplinkBandwidth3 = edge["bandwidth_up"]
+        DownlinkBandwidth3 = edge["bandwidth_down"]
     IpNoSub3 = Ip3.split("/")[0]
     NetmaskAbbr3 = Ip3.split("/")[1]
     
@@ -161,6 +202,30 @@ def writeRouter(f,Router, edges):
     f.write("exit\n")
     f.write("ip forwarding\n")
     f.write("exit'\n")
+    
+    if UplinkBandwidth1 > 0 or DownlinkBandwidth1 > 0:
+      f.write('sudo ./wondershaper -a ' + Interface1)
+      if DownlinkBandwidth1 > 0:
+        f.write(' -d ' + str(DownlinkBandwidth1))
+      if UplinkBandwidth1 > 0:
+        f.write(' -u ' + str(UplinkBandwidth1))
+      f.write('\n')
+
+    if UplinkBandwidth2 > 0 or DownlinkBandwidth2 > 0:
+      f.write('sudo ./wondershaper -a ' + Interface2)
+      if DownlinkBandwidth2 > 0:
+        f.write(' -d ' + str(DownlinkBandwidth2))
+      if UplinkBandwidth2 > 0:
+        f.write(' -u ' + str(UplinkBandwidth2))
+      f.write('\n')
+    
+    if UplinkBandwidth3 > 0 or DownlinkBandwidth3 > 0:
+      f.write('sudo ./wondershaper -a ' + Interface3)
+      if DownlinkBandwidth3 > 0:
+        f.write(' -d ' + str(DownlinkBandwidth3))
+      if UplinkBandwidth3 > 0:
+        f.write(' -u ' + str(UplinkBandwidth3))
+      f.write('\n')
     f.write(CustumScript + " \n") #here there is the custum script
     f.write("echo \"Configuration END\"\n")
     f.write("echo \"" + Name + " is ready to Use\"\n")
